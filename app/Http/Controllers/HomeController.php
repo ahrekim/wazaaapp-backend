def89fc7ede28ad1d1e7ade857eb31f7dc5c460e
Kim Ahrenberg <kim.ahrenberg@gmail.com>
2021-09-14 07:17:29 +0300

    Use homecontroller for lander (and the only) page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\TranslateHelper;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
-     * Show the application dashboard.
+     * Show the home page
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
